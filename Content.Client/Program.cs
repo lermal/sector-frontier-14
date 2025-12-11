d1755fe5fca4d31a3e8d7966380c5608bb837799
HacksLua <parkyrist.ak@gmail.com>
2025-12-11 17:10:05 +0700

    Новогодняя движуха

--- Content.Client/Program.cs
+++ Content.Client/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using Robust.Client;
+using Robust.Shared.Utility;
 
 namespace Content.Client
 {
@@ -7,7 +9,25 @@ namespace Content.Client
         [STAThread]
         public static void Main(string[] args)
         {
-            ContentStart.Start(args);
+            if (IsHolidaySeason(DateTime.Now))
+            {
+                var options = new GameControllerOptions
+                {
+                    WindowIconSet = new ResPath(@"/Textures/_Lua/Logo/christmas_icon"),
+                    SplashLogo = new ResPath(@"/Textures/_Lua/Logo/christmas_logo.png"),
+                };
+                ContentStart.StartLibrary(args, options);
+            }
+            else
+            {
+                ContentStart.Start(args);
+            }
+        }
+        private static bool IsHolidaySeason(DateTime now)
+        {
+            var month = now.Month;
+            var day = now.Day;
+            return (month == 12 && day >= 1) || (month == 1 && day <= 15);
         }
     }
 }
